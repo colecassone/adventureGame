cfc115a60bfdb51cc22ee8aa34c7dc0ff5d4b926
colecass651 <colecass651@SDS-L-F81HC63>
2022-04-01 21:18:03 -0400

    Add project files.

---
+++ adventureGame/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace adventureGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.leftButton = new System.Windows.Forms.Button();
+            this.rightButton = new System.Windows.Forms.Button();
+            this.middleButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.Label();
+            this.imageOutput = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // leftButton
+            // 
+            this.leftButton.BackColor = System.Drawing.Color.LightGray;
+            this.leftButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leftButton.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.leftButton.Location = new System.Drawing.Point(-1, 269);
+            this.leftButton.Name = "leftButton";
+            this.leftButton.Size = new System.Drawing.Size(161, 100);
+            this.leftButton.TabIndex = 0;
+            this.leftButton.Text = "START";
+            this.leftButton.UseVisualStyleBackColor = false;
+            this.leftButton.Click += new System.EventHandler(this.leftButton_Click);
+            // 
+            // rightButton
+            // 
+            this.rightButton.BackColor = System.Drawing.Color.LightGray;
+            this.rightButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rightButton.Location = new System.Drawing.Point(374, 269);
+            this.rightButton.Name = "rightButton";
+            this.rightButton.Size = new System.Drawing.Size(164, 100);
+            this.rightButton.TabIndex = 1;
+            this.rightButton.Text = "END GAME";
+            this.rightButton.UseVisualStyleBackColor = false;
+            this.rightButton.Click += new System.EventHandler(this.rightButton_Click);
+            // 
+            // middleButton
+            // 
+            this.middleButton.BackColor = System.Drawing.Color.LightGray;
+            this.middleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.middleButton.Location = new System.Drawing.Point(181, 269);
+            this.middleButton.Name = "middleButton";
+            this.middleButton.Size = new System.Drawing.Size(171, 100);
+            this.middleButton.TabIndex = 2;
+            this.middleButton.UseVisualStyleBackColor = false;
+            this.middleButton.Visible = false;
+            this.middleButton.Click += new System.EventHandler(this.middleButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(60, 13);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(395, 33);
+            this.outputLabel.TabIndex = 3;
+            this.outputLabel.Text = "Last Night\'s Adventure Game";
+            this.outputLabel.Click += new System.EventHandler(this.textBox_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(247, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxInput.Location = new System.Drawing.Point(118, -3);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(280, 104);
+            this.textBoxInput.TabIndex = 5;
+            this.textBoxInput.Text = "label2";
+            this.textBoxInput.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // imageOutput
+            // 
+            this.imageOutput.Location = new System.Drawing.Point(137, 121);
+            this.imageOutput.Name = "imageOutput";
+            this.imageOutput.Size = new System.Drawing.Size(246, 132);
+            this.imageOutput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageOutput.TabIndex = 6;
+            this.imageOutput.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(533, 367);
+            this.Controls.Add(this.imageOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.middleButton);
+            this.Controls.Add(this.rightButton);
+            this.Controls.Add(this.leftButton);
+            this.Name = "Form1";
+            this.Text = "Choose Your Own Adventure";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button leftButton;
+        private System.Windows.Forms.Button rightButton;
+        private System.Windows.Forms.Button middleButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label textBoxInput;
+        private System.Windows.Forms.PictureBox imageOutput;
+    }
+}
+
---
+++ adventureGame/Form1.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace adventureGame
+{
+    public partial class Form1 : Form
+    {
+        int page = 1;
+        int randomNumber;
+        Random randGen = new Random();
+       
+        public Form1()
+
+        {
+            InitializeComponent();
+            DisplayPage();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void leftButton_Click(object sender, EventArgs e)
+        {
+            // left button outcome 
+            if (page == 1)
+            {
+                page = 2;
+                
+            }
+            else if (page == 2)
+            {
+                page = 4; 
+            }
+            
+            else if (page == 5)
+            {
+               page = 8; 
+             }
+            else if (page == 100)
+            {
+                page = 2; 
+            }
+            else if (page == 99)
+            {
+                page = 2; 
+            }
+           
+            else if (page == 3)
+            {
+                page = 7; 
+            }
+            else if (page == 8)
+            {
+                page = 9; 
+            }
+            else if (page ==7)
+            {
+                page = 10;
+            }
+       
+            DisplayPage();
+        }
+
+        private void rightButton_Click(object sender, EventArgs e)
+        {
+            // right button outcome 
+            if (page == 1)
+            {
+                this.Close();
+            }
+            else if (page == 2)
+            {
+                randomNumber = randGen.Next(1, 6);
+                if (randomNumber == 5)
+                {
+                    page = 100;
+
+                }
+                else
+                {
+                    page = 99;
+                }
+
+
+            }
+            else if (page == 5)
+            {
+                randomNumber = randGen.Next(1, 6);
+                if (randomNumber == 5)
+                {
+                    page = 100;
+
+                }
+                else
+                {
+                    page = 99;
+                }
+
+            }
+            else if (page == 4)
+            {
+                page = 6; 
+            }
+           else if (page == 100)
+            {
+                this.Close(); 
+            }
+            else if (page ==99)
+            {
+                this.Close(); 
+            }
+            else if (page == 3)
+            {
+                page = 5; 
+            }
+            else if (page == 7)
+            {
+                page = 5;
+            }
+            else if (page == 8)
+            {
+                page = 11; 
+            }
+            DisplayPage();
+        }
+        private void middleButton_Click(object sender, EventArgs e)
+        {
+            // middle button outcome 
+            if (page == 2)
+            {
+                page = 3; 
+            }
+            else if (page== 6)
+            {
+                page = 99; 
+            }
+            else if (page == 9)
+            {
+                page = 100; 
+            }
+            else if (page == 10)
+            {
+                randomNumber = randGen.Next(1, 6);
+                if (randomNumber == 5)
+                {
+                    page = 100;
+
+                }
+                else
+                {
+                    page = 99;
+                }
+
+            }
+            else if (page == 11)
+            {
+                page = 99; 
+            }
+            DisplayPage();
+        }
+        public void DisplayPage()
+        {
+            switch (page)
+            {
+               // start game 
+                case 1:
+                    imageOutput.Image = Properties.Resources.PG1;
+                    outputLabel.Text = "Last Night's Adventure Game";
+                    leftButton.Text = "Start";
+                    rightButton.Text = "END GAME";
+                    textBoxInput.Visible = false;
+                    break;
+                    // when the start to the adveture 
+                    case 2:
+                    imageOutput.Image = Properties.Resources.PG2;
+                    outputLabel.Visible = false;
+                    textBoxInput.Visible = true;
+                    middleButton.Visible = true;
+                    leftButton.Text = $"Call phone number you have in your pocket";
+                    rightButton.Text = $"You go home to your wife and don't tell her a thing";
+                    middleButton.Text = $"Walk around and see where you are and how you might end up there";
+                    textBoxInput.Text = $"You just woke up on the streets of downtown Toronto and don't remember a thing you did last night";
+                    textBoxInput.Text += $"\n\n What do you do? "; 
+                    break;
+                    // when you are walking around page too see what happened last night 
+                    case 3:
+                    imageOutput.Image = Properties.Resources.PG3;
+                    textBoxInput.Text = $"You walk around for a bit then you  find an alleyway that you remember from last night";
+                    textBoxInput.Text += $"\n\n What do you do? ";
+                    leftButton.Text = $"Keep looking around";
+                    middleButton.Text = $"OR";
+                    rightButton.Text = $"Go down it"; 
+                    break;
+                    //when you call the phone number page 
+                    case 4:
+                    imageOutput.Image = Properties.Resources.Good_PG4;
+                    textBoxInput.Text = $"A girl picks up the phone and says \"hey SEXY when are you coming home to your new wife\" you say? ";
+                  
+                    leftButton.Text = "Can we meet and talk about what happened last night?";
+                    middleButton.Text = $"OR";
+                    rightButton.Text = $"Hey I am married. Who do you think you are talking to!"; 
+                    break;
+                    // when you go down the alliway page 
+                case 5:
+                    SoundPlayer ranGuy = new SoundPlayer(Properties.Resources.A99);
+                    ranGuy.Play();
+                    imageOutput.Image = Properties.Resources.PG5;
+                    textBoxInput.Text = $"You are going down then out of the blue someone calls you by name and says \"you and Jessica had a great weding\" and you are confused because that is not your wife's name. Then you realized what really happened that night!";
+                    middleButton.Text = $"OR";
+                    rightButton.Text = $"Deny what he says and go home";
+                    leftButton.Text = $"Find out who Jessica is and go talk to her"; 
+                    break;
+                    //the outcome when you call the phone # and say " i am married"
+                case 6:
+                    imageOutput.Image = Properties.Resources.Good_PG6;
+                    leftButton.Visible = false;
+                    rightButton.Visible = false;
+                    middleButton.Visible = false;
+                    textBoxInput.Text = $"She yells \" YOUR MARRIED!! your wife is going to love to here what we did last night\" then she hangs up the the phone \n \n So when you got home...";
+                    this.Refresh();
+                    Thread.Sleep(2000);
+                    middleButton.Visible = true;
+                    middleButton.Text = $"PUSH BOTTON TO SEE WHAT HAPPENS";
+                    page = 99;
+                    break;
+                    //this is the outcome of you keep looking around
+                case 7:
+                    imageOutput.Image = Properties.Resources.PG7;
+                    middleButton.Text = $"OR";
+                    textBoxInput.Text = $"You keep looking for a long time and don't recognized anything else. Do you?";
+                    rightButton.Text = $"Go back to the alleyway";
+                    leftButton.Text = $"Give up and go home"; 
+                    break;
+                    //this is what happens when you see Jessica
+                case 8:
+                    imageOutput.Image = Properties.Resources.PG8;
+                    leftButton.Text = $"Explain how sorry you are and say you were married to another person";
+                    middleButton.Text = $"OR";
+                    rightButton.Text = $"You say we are no longer married very angrily and leave and go home!";
+                    textBoxInput.Text = "You find Jessica and you?"; 
+                    break;
+                    // when you cover up what happened 
+                case 9:
+                    leftButton.Visible = false;
+                    rightButton.Visible = false;
+                    middleButton.Visible = false;
+                    textBoxInput.Text = $"She agrees to get divorce and not tell anyone what really happend that night then"; 
+                   this.Refresh();
+                    Thread.Sleep(1500);
+                    middleButton.Visible = true;
+                    middleButton.Text = $"Click here to see what happens nexts";
+                    break;
+                    // going home to see what happens 
+                case 10:
+                    leftButton.Visible = false;
+                    rightButton.Visible = false;
+                    middleButton.Visible = false;
+                    textBoxInput.Text = $"you go home and...";
+                    this.Refresh();
+                    Thread.Sleep(1500);
+                    middleButton.Visible = true;
+                    middleButton.Text = $"Click here to see what happens nexts";
+                    break;
+                    case 11:
+                    leftButton.Visible = false;
+                    rightButton.Visible = false;
+                    middleButton.Visible = false; 
+                    textBoxInput.Text = $"You go home this happens...";
+                    this.Refresh();
+                    Thread.Sleep(1500);
+                    middleButton.Visible = true;
+     
+                    middleButton.Text = $"Click here to see what happens nexts";
+                    break;
+                //This is the good ending 
+                case 100:
+                    SoundPlayer youWin = new SoundPlayer(Properties.Resources.A100);
+                    youWin.Play();
+                    leftButton.Visible = true;
+                    rightButton.Visible = true;
+                    middleButton.Visible = true;
+                    imageOutput.Image = Properties.Resources.PG100;
+                    textBoxInput.Text = $"You go home to your wife and live happily ever after and she never new what happend that day";
+                    leftButton.Text = "";
+                    middleButton.Text = "";
+                    rightButton.Text = "";
+                    rightButton.BackColor = Color.Green;
+                    leftButton.BackColor = Color.Green;
+                    middleButton.BackColor = Color.Green;
+                    this.Refresh();
+                    Thread.Sleep(5000);
+                    rightButton.BackColor = Color.LightGray;
+                    leftButton.BackColor = Color.LightGray;
+                    middleButton.BackColor = Color.LightGray;
+                    textBoxInput.Text = $"DO YOU WANT TO PLAY AGAIN";
+                    leftButton.Text = "Yes";
+                    middleButton.Text = "OR";
+                    rightButton.Text = "No";
+                    break;
+                    // this is the bad ending 
+                case 99:
+                    SoundPlayer youlose = new SoundPlayer(Properties.Resources.A999);
+                    youlose.Play(); 
+                    leftButton.Visible = true;
+                    rightButton.Visible = true;
+                    middleButton.Visible = true; 
+                    imageOutput.Image = Properties.Resources.PG99;
+                    leftButton.Visible = true;
+                    rightButton.Visible = true;
+                    middleButton.Visible = true;
+                    textBoxInput.Text = $"Your wife finds out what happened that night and you get a divorce then die of loneliness";
+                    leftButton.Text = "";
+                    middleButton.Text = "";
+                    rightButton.Text = "";
+                    rightButton.BackColor = Color.Red;
+                    leftButton.BackColor = Color.Red;
+                    middleButton.BackColor = Color.Red; 
+                    this.Refresh();
+                    Thread.Sleep(5000);
+                    textBoxInput.Text = $"DO YOU WANT TO PLAY AGAIN";
+                    leftButton.Text = "Yes";
+                    middleButton.Text = "OR";
+                    rightButton.Text = "No";
+                    rightButton.BackColor = Color.LightGray;
+                    leftButton.BackColor = Color.LightGray;
+                    middleButton.BackColor = Color.LightGray;
+                    break;
+            }
+        }
+
+        
+    }
+}
